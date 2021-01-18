4733d23682ea564253bbf50fa205c666e52cddef
Rodrigo Carneiro <carneironline@gmail.com>
2021-01-18 01:00:50 -0300

    upload de arquivos

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,35 +1,36 @@
-import { startOfHour } from 'date-fns'
-import { getCustomRepository } from 'typeorm'
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
-import Appointment from '../models/Appointments'
-import AppointmentsRepository from '../repositories/AppointmentsRepository'
+import Appointment from '../models/Appointments';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppoinmentService {
+  public async execute({ date, provider_id }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    const appointmentDate = startOfHour(date);
 
-  public async execute({ date, provider }: Request): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository)
-    const appointmentDate = startOfHour(date)
-
-    const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate)
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
 
     if (findAppointmentInSameDate) {
-      throw Error('This appointment is already booked')
+      throw Error('This appointment is already booked');
     }
 
     const appointment = appointmentsRepository.create({
-      provider,
-      date: appointmentDate
-    })
+      provider_id,
+      date: appointmentDate,
+    });
 
-    await appointmentsRepository.save(appointment)
+    await appointmentsRepository.save(appointment);
 
-    return appointment
+    return appointment;
   }
 }
 
-export default CreateAppoinmentService
+export default CreateAppoinmentService;
